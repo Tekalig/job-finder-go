649247a406702557e042b543f971ecfc5688d49d
Tekalig <tekahazi06@gmail.com>
2025-05-29 15:16:13 +0300

    Refactor hasura client code for consistent formatting and indentation

--- hasura/client.go
+++ hasura/client.go
@@ -2,49 +2,49 @@
 package hasura
 
 import (
-    "bytes"
-    "encoding/json"
-    "net/http"
+	"bytes"
+	"encoding/json"
+	"net/http"
 )
 
 type Client struct {
-    endpoint    string
-    adminSecret string
-    httpClient  *http.Client
+	endpoint    string
+	adminSecret string
+	httpClient  *http.Client
 }
 
 func NewClient(endpoint, adminSecret string) *Client {
-    return &Client{
-        endpoint:    endpoint,
-        adminSecret: adminSecret,
-        httpClient:  &http.Client{},
-    }
+	return &Client{
+		endpoint:    endpoint,
+		adminSecret: adminSecret,
+		httpClient:  &http.Client{},
+	}
 }
 
 func (c *Client) Execute(query string, variables map[string]interface{}, result interface{}) error {
-    payload := map[string]interface{}{
-        "query":     query,
-        "variables": variables,
-    }
-    
-    jsonData, err := json.Marshal(payload)
-    if err != nil {
-        return err
-    }
-    
-    req, err := http.NewRequest("POST", c.endpoint, bytes.NewBuffer(jsonData))
-    if err != nil {
-        return err
-    }
-    
-    req.Header.Set("Content-Type", "application/json")
-    req.Header.Set("x-hasura-admin-secret", c.adminSecret)
-    
-    resp, err := c.httpClient.Do(req)
-    if err != nil {
-        return err
-    }
-    defer resp.Body.Close()
-    
-    return json.NewDecoder(resp.Body).Decode(result)
+	payload := map[string]interface{}{
+		"query":     query,
+		"variables": variables,
+	}
+
+	jsonData, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", c.endpoint, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("x-hasura-admin-secret", c.adminSecret)
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return json.NewDecoder(resp.Body).Decode(result)
 }
