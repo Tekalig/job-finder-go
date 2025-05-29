687c49143fad72673dbf949564ba77796648dc4f
Tekalig <tekahazi06@gmail.com>
2025-05-29 15:15:28 +0300

    Refactor config.go for consistent formatting and indentation

--- config/config.go
+++ config/config.go
@@ -2,30 +2,31 @@
 package config
 
 import (
-    "os"
-    "github.com/joho/godotenv"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
-    HasuraEndpoint    string
-    HasuraAdminSecret string
-    JWTSecret        string
-    SMTPHost         string
-    SMTPPort         string
-    SMTPUser         string
-    SMTPPassword     string
+	HasuraEndpoint    string
+	HasuraAdminSecret string
+	JWTSecret         string
+	SMTPHost          string
+	SMTPPort          string
+	SMTPUser          string
+	SMTPPassword      string
 }
 
 func Load() (*Config, error) {
-    godotenv.Load()
+	godotenv.Load()
 
-    return &Config{
-        HasuraEndpoint:    os.Getenv("HASURA_ENDPOINT"),
-        HasuraAdminSecret: os.Getenv("HASURA_ADMIN_SECRET"),
-        JWTSecret:        os.Getenv("JWT_SECRET"),
-        SMTPHost:         os.Getenv("SMTP_HOST"),
-        SMTPPort:         os.Getenv("SMTP_PORT"),
-        SMTPUser:         os.Getenv("SMTP_USER"),
-        SMTPPassword:     os.Getenv("SMTP_PASSWORD"),
-    }, nil
+	return &Config{
+		HasuraEndpoint:    os.Getenv("HASURA_ENDPOINT"),
+		HasuraAdminSecret: os.Getenv("HASURA_ADMIN_SECRET"),
+		JWTSecret:         os.Getenv("JWT_SECRET"),
+		SMTPHost:          os.Getenv("SMTP_HOST"),
+		SMTPPort:          os.Getenv("SMTP_PORT"),
+		SMTPUser:          os.Getenv("SMTP_USER"),
+		SMTPPassword:      os.Getenv("SMTP_PASSWORD"),
+	}, nil
 }
