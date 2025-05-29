d4c43f2d9556f9582a41eb2a95144aff9d34dee7
Tekalig <tekahazi06@gmail.com>
2025-05-29 15:16:22 +0300

    Refactor SetupRoutes to pass hasuraClient to handler functions for improved dependency management

--- routes/routes.go
+++ routes/routes.go
@@ -3,28 +3,43 @@ package routes
 import (
 	"github.com/Tekalig/job-finder-go/config"
 	"github.com/Tekalig/job-finder-go/handlers"
+	"github.com/Tekalig/job-finder-go/hasura"
 	"github.com/Tekalig/job-finder-go/middleware"
 	"github.com/gin-gonic/gin"
 )
 
-func SetupRoutes(r *gin.Engine, cfg *config.Config) {
+func SetupRoutes(r *gin.Engine, cfg *config.Config, hasuraClient *hasura.Client) {
 	api := r.Group("/api")
 
 	// Public routes
 	auth := api.Group("/auth")
 	{
-		auth.POST("/employer/signup", handlers.EmployerSignup)
-		auth.POST("/expert/signup", handlers.ExpertSignup)
-		auth.POST("/login", handlers.Login)
+		auth.POST("/employer/signup", func(c *gin.Context) {
+			handlers.EmployerSignup(c, hasuraClient)
+		})
+		auth.POST("/expert/signup", func(c *gin.Context) {
+			handlers.ExpertSignup(c, hasuraClient)
+		})
+		auth.POST("/login", func(c *gin.Context) {
+			handlers.Login(c, hasuraClient, cfg.JWTSecret)
+		})
 	}
 
 	// Protected routes
 	protected := api.Group("/")
 	protected.Use(middleware.Auth(cfg.JWTSecret))
 	{
-		protected.POST("/jobs", handlers.CreateJob)
-		protected.GET("/jobs", handlers.GetJobs)
-		protected.PUT("/jobs/:id", handlers.UpdateJob)
-		protected.DELETE("/jobs/:id", handlers.DeleteJob)
+		protected.POST("/jobs", func(c *gin.Context) {
+			handlers.CreateJobHandler(c, hasuraClient)
+		})
+		protected.GET("/jobs", func(c *gin.Context) {
+			handlers.GetJobsHandler(c, hasuraClient)
+		})
+		protected.PUT("/jobs/:id", func(c *gin.Context) {
+			handlers.UpdateJobHandler(c, hasuraClient)
+		})
+		protected.DELETE("/jobs/:id", func(c *gin.Context) {
+			handlers.DeleteJobHandler(c, hasuraClient)
+		})
 	}
 }
